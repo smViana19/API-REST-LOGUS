09549534c109c927cee26feebb4b87d119dc9067
Samuu <samuelfilipevianadois@gmail.com>
2024-07-14 12:48:21 -0300

    Corrigido e adicionado a configuracao de rotas das tasks

--- src/app.js
+++ src/app.js
@@ -1,36 +1,36 @@
-import dotenv from 'dotenv';
-import { resolve } from 'path';
-
+import dotenv from "dotenv";
+import { resolve } from "path";
 
 dotenv.config();
-import './database'
+import "./database";
 import express from "express";
-import cors from 'cors';
-import helmet from 'helmet';
+import cors from "cors";
+import helmet from "helmet";
 
 import homeRoutes from "./routes/homeRoutes";
 import userRoutes from "./routes/userRoutes";
 import tokenRoutes from "./routes/tokenRoutes";
 import alunoRoutes from "./routes/alunoRoutes";
 import fotoRoutes from "./routes/fotoRoutes";
+import taskRoutes from "./routes/taskRoutes";
 
 //PARA UTILIZAR A API
 const whiteList = [
-  'http://localhost:5174',
-  'http://localhost:3000',
-  'http://localhost:5173',
-  'http://localhost:5175'
+  "http://localhost:5174",
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:5175",
 ];
 
 const corOptions = {
   origin: function (origin, callback) {
     //se a URI tiver nas opçoes de whiteList
-    if(whiteList.indexOf(origin) !== -1 || !origin) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-  }
+  },
 };
 class App {
   constructor() {
@@ -46,17 +46,17 @@ class App {
     //metodo das middlewares
     this.App.use(express.urlencoded({ extended: true })); //configuracao do express
     this.App.use(express.json());
-    this.App.use(express.static(resolve(__dirname, 'uploads')));
+    this.App.use(express.static(resolve(__dirname, "uploads")));
   }
 
   routes() {
     //metodo das rotas
-    this.App.use('/', homeRoutes);
-    this.App.use('/users/', userRoutes);
-    this.App.use('/tokens/', tokenRoutes);
-    this.App.use('/alunos/', alunoRoutes);
-    this.App.use('/fotos/', fotoRoutes);
-
+    this.App.use("/", homeRoutes);
+    this.App.use("/users/", userRoutes);
+    this.App.use("/tokens/", tokenRoutes);
+    this.App.use("/alunos/", alunoRoutes);
+    this.App.use("/fotos/", fotoRoutes);
+    this.App.use("/tasks/", taskRoutes);
   }
 }
 
