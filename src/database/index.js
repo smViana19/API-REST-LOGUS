993dd7726646bac1f133d61c69460fb29868d8f1
Samuu <samuelfilipevianadois@gmail.com>
2024-07-14 12:47:14 -0300

    Adicionado Taks nas cofniguracoes do sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,10 @@ import databaseConfig from "../config/database";
 import Aluno from "../models/Aluno";
 import User from "../models/User";
 import Foto from "../models/Foto";
+import Task from "../models/Task";
 
 //se caso tiver mais model importa e colocar no array
-const models = [Aluno, User, Foto];
+const models = [Aluno, User, Foto, Task];
 
 const connection = new Sequelize(databaseConfig);
 
