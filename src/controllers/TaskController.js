ce8e39c6d79d182097710ab66106184d317d1507
Samuu <samuelfilipevianadois@gmail.com>
2024-07-14 15:53:53 -0300

    Adicionado metodos de update e delete

--- src/controllers/TaskController.js
+++ src/controllers/TaskController.js
@@ -3,15 +3,55 @@ import Task from "../models/Task";
 class TaskController {
   async store(req, res) {
     try {
-      console.log("Corpo da requisição:", req.body);
       const task = await Task.create(req.body);
-      console.log("Tarefa criada:", task);
-      return res.json(task);
+      const taskWithUser = await Task.findByPk(task.id, { include: 'user' });
+      return res.json(taskWithUser);
+
     } catch (e) {
       console.error("Erro ao criar tarefa:", e);
       return res.status(400).json({
         errors: ["Erro ao criar tarefa"],
       });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const task = await Task.findByPk(req.params.id)
+      if (!task) {
+        return res.status(400).json({
+          errors: ["Tarefa nao existe."],
+        });
+      }
+      const newTask = await task.update(req.body)
+      const { id, titulo, categoria, descricao, urgencia } = newTask;
+      return res.json({id, titulo, categoria, descricao, urgencia})
+
+    } catch (e) {
+      console.error("Error editar a tarefa:", e)
+      return res.status(400).json({
+        errors: ["Error ao editar tarefa"],
+      })
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const task = await Task.findByPk(req.params.id)
+      if (!task) {
+        return res.status(400).json({
+          errors: ["Tarefa nao existe."]
+        });
+      }
+      await task.destroy()
+      return res.json({
+        message: ["Deletada com sucesso"]
+      });
+    } catch (e) {
+      console.error("Error excluir a tarefa:", e)
+      return res.status(400).json({
+        errors: ["Error ao excluir tarefa"],
+      })
     }
   }
 }
