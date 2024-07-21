fb8d584fba8fe7f598f3fba5e8dc0029feee9f62
Samuu <samuelfilipevianadois@gmail.com>
2024-07-20 22:11:38 -0300

    refactor: refatorado models consumidas

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,11 @@ import Aluno from "../models/Aluno";
 import User from "../models/User";
 import Foto from "../models/Foto";
 import Task from "../models/Task";
+import Subject from "../models/Subject";
+import SubjectMaterial from "../models/SubjectMaterial";
 
-//se caso tiver mais model importa e colocar no array
-const models = [Aluno, User, Foto, Task];
+//se caso tiver mais model importa e colocar nessa lista
+const models = [Aluno, User, Foto, Task, Subject, SubjectMaterial];
 
 const connection = new Sequelize(databaseConfig);
 
