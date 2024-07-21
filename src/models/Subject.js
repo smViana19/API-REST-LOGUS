eca5792de2dab452b3c018fb16233b247bd6ad44
Samuu <samuelfilipevianadois@gmail.com>
2024-07-20 22:12:57 -0300

    fix(feat): Corrigir e adicionar as chaves estrangeiras do banco

--- src/models/Subject.js
+++ src/models/Subject.js
@@ -11,15 +11,19 @@ export default class Subject extends Model{
             len: {
               args: [4, 255],
               msg: "Nome da materia precisa ter mais de 4 caracteres",
-            }
-          }
+            },
+          },
         }
       },
       {
-        sequelize
+        sequelize,
+        timestamps: false,
       },
     );
     return this;
   }
-
+  static associate(models) {
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.hasMany(models.SubjectMaterial, { foreignKey: "subject_id", as: "materials" });
+  }
 }
