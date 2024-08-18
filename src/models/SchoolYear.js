d4313ce65b5802b6fa9b8b4aa5b0802719bae0ee
Samuu <samuelfilipevianadois@gmail.com>
2024-08-18 12:05:23 -0300

    Adicionado model dos Anos escolares
    - Criação e configuração da model

---
+++ src/models/SchoolYear.js
@@ -0,0 +1,41 @@
+import Sequelize, {Model} from "sequelize";
+
+export default class SchoolYear extends Model {
+  static init(sequelize) {
+    super.init({
+        ano: {
+          type: Sequelize.INTEGER,
+          validate: {
+            isInt: true,
+            len: [4, 4],
+            msg: "Ano inválido."
+          },
+        },
+        data_inicio: {
+          type: Sequelize.DATE,
+          validate: {
+            allowNull: true,
+          },
+
+        },
+        data_fim: {
+          type: Sequelize.DATE,
+          validate: {
+            allowNull: true,
+          },
+        },
+      },
+      {
+        sequelize,
+        modelName: "SchoolYear",
+        tableName: "anos_escolares",
+        timestamps: false
+      },
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.hasMany(models.SchoolYearGrade, { foreignKey: "ano_escolar_id"})
+  }
+}
