6dc1df385c991398e24e1d008ea118520e45636a
Samuu <samuelfilipevianadois@gmail.com>
2024-08-18 12:05:40 -0300

    Adicionado model dos Anos escolares
    - Criação e configuração da model
    - Associação entre as tabelas referentes

---
+++ src/models/SchoolYearGrade.js
@@ -0,0 +1,25 @@
+import Sequelize, {Model} from "sequelize";
+
+export default class SchoolYearGrade extends Model {
+  static init(sequelize) {
+    super.init({
+        turma: {
+          type: Sequelize.STRING,
+          allowNull: false
+        } //se caso tiver por exemplo mais de uma turma exemplo A,B,C
+      },
+      {
+        sequelize,
+        modelName: "SchoolYearGrade",
+        tableName: "ano_series",
+        timestamps: false
+      },
+    );
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Grade, { foreignKey: 'serie_id', as: 'serie' });
+    this.belongsTo(models.SchoolYear, { foreignKey: 'ano_escolar_id', as: 'ano_escolar' });
+  }
+}
