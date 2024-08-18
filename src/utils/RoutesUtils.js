4e5d15798f666f8b3fc9506cc84d97ac950c4419
Samuu <samuelfilipevianadois@gmail.com>
2024-08-18 12:43:53 -0300

    Adicionado rota URL utils
    -Criacao do endpoint de administrador para gerencia das escolas

--- src/utils/RoutesUtils.js
+++ src/utils/RoutesUtils.js
@@ -6,9 +6,7 @@ export const URL_TOKENS_ROUTE = "/tokens/"
 // export const URL_ALUNO_ROUTE = "/alunos/"
 export const URL_TASKS_ROUTE = "/tarefas"
 export const URL_SUBJECTS_ROUTE = "/materias/"
-export const URl_SUBJECTS_MATERIALS_ROUTE = "/materias/material"
+export const URl_SUBJECTS_MATERIALS_ROUTE = "/materias/material/"
 
-
-
-
+export const URL_SCHOOL_YEAR_ROUTE = "/admin/ano-escolar/"
 
