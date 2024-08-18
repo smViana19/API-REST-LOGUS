22f1a54f4bb735045dcf14cdf52309496deae594
Samuu <samuelfilipevianadois@gmail.com>
2024-08-18 12:43:19 -0300

    Removido Post
    -Remoçao temporaria

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,9 +1,24 @@
-import {Model} from "sequelize";
+import Sequelize, {Model} from "sequelize";
 
 export default class Post extends Model  {
-  static init(sequelize){
+  static init(sequelize) {
     super.init({
-      
-    })
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: "",
+          validate: {
+            len: {
+              args: [4, 255],
+              msg: "Nome da materia precisa ter mais de 4 caracteres",
+            },
+          },
+        }
+      },
+      {
+        sequelize,
+        timestamps: false,
+      },
+    );
+    return this;
   }
 }
