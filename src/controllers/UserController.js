67b27ee0c4e830f10c4e599005dc8587f964d128
Samuu <samuelfilipevianadois@gmail.com>
2024-09-04 19:32:21 -0300

    modificado respostas de erro
    - Corrigido mensagem de erro ao criar usuario quando ele já existe

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,39 +5,25 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      const { id, nome, email, role } = novoUser;
-      return res.json({ id, nome, email, role }); //adicionado role
+      const {id, nome, email, role} = novoUser;
+
+      return res.json({id, nome, email, role}); //adicionado role
     } catch (e) {
       console.log("error: ", e)
       return res.status(400).json({
-        errors: ["deu erro "],
-
+        errors: ["Erro ao criar usuario"],
       });
     }
   }
 
-  // Index
-  // async index(req, res) {
-  //   try {
-  //     const users = await User.findAll({
-  //       attributes: ["id", "nome", "email", "role"],
-  //     });
-  //     return res.json(users);
-  //   } catch (e) {
-  //     return res.json(null);
-  //   }
-  // }
-
-  // Show
   async index(req, res) {
     try {
       const users = await User.findAll({
         attributes: ["id", "nome", "email", "role"],
       });
-      console.log("Usuários encontrados:", users); // Adicionando log para depuração
       return res.json(users);
     } catch (e) {
-      console.error("Erro ao listar usuários:", e); // Adicionando log para depuração
+      console.error("Erro ao listar usuários:", e);
       return res.status(500).json({
         errors: ["Erro ao listar usuários."],
       });
@@ -47,25 +33,26 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      const { id, nome, email, role } = user;
-      return res.json({ id, nome, email, role });
+      const {id, nome, email, role} = user;
+      return res.json({id, nome, email, role});
     } catch (e) {
       return res.json(null);
     }
   }
+
   // Update
   async update(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
       if (!user) {
         return res.status(400).json({
-          errors: ["Usuario nao existe."],
+          errors: ["Usuario não encontrado"],
         });
       }
 
       const novosDados = await user.update(req.body);
-      const { id, nome, email, role } = novosDados;
-      return res.json({ id, nome, email, role });
+      const {id, nome, email, role} = novosDados;
+      return res.json({id, nome, email, role});
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -79,7 +66,7 @@ class UserController {
       const user = await User.findByPk(req.params.id);
       if (!user) {
         return res.status(400).json({
-          errors: ["Usuario nao existe."],
+          errors: ["Usuario não existe."],
         });
       }
 
@@ -92,4 +79,5 @@ class UserController {
     }
   }
 }
+
 export default new UserController();
