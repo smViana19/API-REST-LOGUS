9fe067bb0fc5cb254a044e1513d1294e357f3ff3
Samuu <samuelfilipevianadois@gmail.com>
2024-09-23 19:17:17 -0300

    refatoração adicao de metodos
    - Adicionado metodos HTTP faltantes na grade controller
    - Ajustado para receber ID nas requisicoes PUT DELETE SHOW

--- src/controllers/GradeController.js
+++ src/controllers/GradeController.js
@@ -23,12 +23,60 @@ class GradeController {
 
     }
   }
+
   async index(req, res) {
     const grade = await Grade.findAll()
     return res.json({
       status: 200,
       grade
     })
+  }
+
+  async show(req, res) {
+    try {
+      const grade = await Grade.findByPk(req.params.id);
+      if (!grade) {
+        return res.status(404).json({error: "Serie não encontrada."})
+      }
+      return res.json(grade)
+
+    } catch (error) {
+      console.error("Erro ao obter a serie escolar: ", error)
+      return res.json({error: "Erro ao obter a serie escolar"})
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const grade = await Grade.findByPk(req.params.id);
+      if (!grade) {
+        return res.status(404).json({error: "Serie não encontrada."})
+      }
+      const {nome, descricao} = req.body
+      const newGrade = await grade.update(nome, descricao);
+      return res.json(newGrade);
+
+    } catch (error) {
+      console.error("Erro ao editar a serie escolar: ", error)
+      return res.status(400).json({error: "Erro ao editar a serie escolar"})
+    }
+  }
+
+  async delete(req, res) {
+    //TODO IMPLEMENTAR ONCASCADE NAS TABELAS
+    try {
+      const grade = await Grade.findByPk(req.params.id);
+      if (!grade) {
+        return res.status(404).json({error: "Serie não encontrada."})
+      }
+      await grade.destroy();
+      return res.json({message: "Deletado com sucesso"})
+    } catch (error) {
+      console.error("Error a serie escolar:", error);
+      return res.status(400).json({
+        error: "Error ao excluir a serie escolar",
+      });
+    }
   }
 
 }
