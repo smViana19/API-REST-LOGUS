fbe66da5efac0fd2d07f848d1a8ca1e6b5b18346
Samuu <samuelfilipevianadois@gmail.com>
2024-09-23 19:17:47 -0300

    adicionado rotas correspondentes
    - Adicionado rotas faltantes dos metodos das controllers

--- src/routes/gradeRoutes.js
+++ src/routes/gradeRoutes.js
@@ -6,6 +6,9 @@ const router = new Router;
 
 router.post('/', gradeController.store)
 router.get('/', gradeController.index)
+router.get('/:id', gradeController.show)
+router.put('/:id', gradeController.update)
+router.delete('/:id', gradeController.delete)
 
 
 export default router;
