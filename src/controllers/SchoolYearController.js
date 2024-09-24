18ae51045be8c345b8fdf3d38e291feae5002616
Samuu <samuelfilipevianadois@gmail.com>
2024-09-24 15:27:50 -0300

    adicionado metodos
    - Adicionado metodos restantes para o CRUD de ano escolar

--- src/controllers/SchoolYearController.js
+++ src/controllers/SchoolYearController.js
@@ -33,13 +33,47 @@ class SchoolYearController {
   }
 
   async show(req, res) {
+    try {
+      const yearSchool = await SchoolYear.findByPk(req.params.id);
+      if (!yearSchool) {
+        return res.status(404).json({error: "Ano escolar nao encontrado."})
+      }
+      return res.json(yearSchool)
+    } catch (error) {
+      console.error("Erro ao obter o ano escolar: ", error)
+      return res.json({error: "Erro ao obter a serie escolar"})
+    }
   }
 
   async update(req, res) {
+    try {
+      const yearSchool = await SchoolYear.findByPk(req.params.id);
+      if (!yearSchool) {
+        return res.status(404).json({error: "Ano escolar nao encontrado."})
+      }
+      const newYearSchool = await yearSchool.update(req.body);
+      return res.json(newYearSchool);
+
+    } catch (error) {
+      console.error("Erro ao editar ano escolar: ", error)
+      return res.status(400).json({error: "Erro ao editar ano escolar"})
+    }
   }
 
   async delete(req, res) {
+    try {
+      const yearSchool = await SchoolYear.findByPk(req.params.id);
+      if (!yearSchool) {
+        return res.status(404).json({error: "Ano escolar nao encontrado."})
+      }
+      await yearSchool.destroy();
+      return res.status(200).json({message: "Deletado com sucesso."})
+    } catch (error) {
+      console.error("Erro ao excluir ano escolar: ", error)
+      return res.json({error: "Erro ao excluir ano escolar"})
+    }
   }
 }
 
 export default new SchoolYearController();
+
--- src/routes/schoolYearRoutes.js
+++ src/routes/schoolYearRoutes.js
@@ -5,6 +5,9 @@ const router = new Router;
 
 router.post('/', SchoolYearController.store)
 router.get('/', SchoolYearController.index)
+router.get('/:id', SchoolYearController.show)
+router.put('/:id', SchoolYearController.update)
+router.delete('/:id', SchoolYearController.delete)
 
 
 export default router;
