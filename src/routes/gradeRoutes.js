6bc073a612b06a525d95946ff5a705fa198bd689
Samuu <samuelfilipevianadois@gmail.com>
2024-09-26 20:03:11 -0300

    adicionado middlewares
    - Configurado rotas para acessar apenas logado e suas roles

--- src/routes/gradeRoutes.js
+++ src/routes/gradeRoutes.js
@@ -1,14 +1,16 @@
 import {Router} from "express";
 import gradeController from "../controllers/GradeController";
+import loginRequired from "../middlewares/loginRequired";
+import roleMiddleware from "../middlewares/roleMiddleware";
 
 
 const router = new Router;
 
-router.post('/', gradeController.store)
-router.get('/', gradeController.index)
-router.get('/:id', gradeController.show)
-router.put('/:id', gradeController.update)
-router.delete('/:id', gradeController.delete)
+router.post('/', loginRequired, roleMiddleware(["diretor"]), gradeController.store)
+router.get('/', loginRequired, gradeController.index)
+router.get('/:id', loginRequired, roleMiddleware(["professor", "diretor"]), gradeController.show)
+router.put('/:id', loginRequired, roleMiddleware(["diretor"]), gradeController.update)
+router.delete('/:id', loginRequired, roleMiddleware(["diretor"]), gradeController.delete)
 
 
 export default router;
