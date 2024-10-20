7c05fee131e6d302007358bbc0af9710815b48f6
Samuu <samuelfilipevianadois@gmail.com>
2024-10-20 15:07:28 -0300

    feat: added new features when registering users - added nodemailer for send emails with password - added new dependency nodemailer

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,28 @@
 /* eslint-disable no-unused-vars */
 import User from "../models/User";
+import {sendEmail} from "../utils/mailUtils";
 
 class UserController {
+
   async store(req, res) {
     try {
+      let { password } = req.body;
+      let passGenerated = true;
+      if(!password) {
+        password = generatePassword();
+        req.body.password = password;
+        passGenerated = true;
+      }
+
       const novoUser = await User.create(req.body);
       const { id, nome, email, role } = novoUser;
+      if(passGenerated) {
+        console.log("Enviando email para: ", email)
+        await sendEmail(email, 'Sua nova conta', `Sua senha é: ${password}`)
+      }
 
-      return res.json({ id, nome, email, role });
+
+      return res.json({message: `Novo ${role} criado com sucesso!`, id, nome, email, role });
     } catch (e) {
       console.log("error: ", e)
       return res.status(400).json({
@@ -70,7 +85,7 @@ class UserController {
       }
 
       await user.destroy();
-      return res.json(null);
+      return res.json({message: "Usuario deletado com sucesso."});
     } catch (e) {
       return res.status(500).json({
         errors: e.errors.map((err) => err.message),
@@ -96,7 +111,19 @@ class UserController {
       return res.status(500).json({ error: "Erro ao contar usuarios." })
     }
   }
+
 }
+
+export const generatePassword = () => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+  let password = '';
+  for (let i = 0; i < 8; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return password;
+};
+
+
 
 
 export default new UserController();
