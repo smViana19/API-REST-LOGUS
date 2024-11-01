47ca5aa735482770787a23c6896c476abd800a1b
Samuu <samuelfilipevianadois@gmail.com>
2024-11-01 16:28:34 -0300

    feat: added new migrations - table notes refactor | removed column trimester and added column fk - create table periods

--- src/controllers/NoteController.js
+++ src/controllers/NoteController.js
@@ -2,17 +2,16 @@ import Note from "../models/Note";
 import SchoolYearGrade from "../models/SchoolYearGrade";
 import User from "../models/User";
 import Subject from "../models/Subject";
+import Period from "../models/Period";
 
 class NoteController {
 
-  async store(req, res) {
+  async createNote(req, res) {
     try {
-      const { anos_series_id, user_id, subject_id, value, trimester } = req.body;
+      const { anos_series_id, user_id, subject_id, value, period_id } = req.body;
       const subjectExists = await Subject.findByPk(subject_id);
       const userExists = await User.findByPk(user_id);
-      if (trimester > 3 || trimester < 1) {
-        return res.status(400).json({ error: "Trimestre inválido." });
-      }
+      const periodExists = await Period.findByPk(period_id)
       if (value < 0) {
         return res.status(400).json({ error: "Valor inválido." })
       }
@@ -22,12 +21,16 @@ class NoteController {
       if (!userExists) {
         return res.status(404).json({ error: "Usuário não encontrado." })
       }
+      if (!periodExists) {
+        return res.status(404).json({ error: "Periodo não encontrado." })
+      }
+
       const note = await Note.create({
         anos_series_id,
         user_id,
         subject_id,
         value,
-        trimester,
+        period_id
       });
       const noteWithAssociations = await Note.findByPk(note.id, {
         include: [
@@ -45,6 +48,11 @@ class NoteController {
             model: Subject,
             as: 'subject',
             attributes: ['id', 'nome']
+          },
+          {
+            model: Period,
+            as: 'period',
+            attributes: ['name']
           }
         ]
       });
@@ -54,7 +62,7 @@ class NoteController {
       return res.status(500).json({ error: "Erro ao criar a nota." });
     }
   }
-  async index(req, res) {
+  async getAllNotes(req, res) {
     try {
       const notes = await Note.findAll({
         include: [
@@ -73,6 +81,11 @@ class NoteController {
             as: 'turma',
             attributes: ['turma'],
           },
+          {
+            model: Period,
+            as: 'period',
+            attributes: ['name']
+          }
         ]
       })
       return res.status(200).json(notes)
@@ -81,9 +94,68 @@ class NoteController {
       return res.status(500).json({ error: "Erro ao listar as notas." })
     }
   }
-  async show() { }
-  async update() { }
-  async delete() { }
+  async getNoteById(req, res) {
+    try {
+      const note = await Note.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            as: 'user',
+            attributes: ['id', 'nome']
+          },
+          {
+            model: Subject,
+            as: 'subject',
+            attributes: ['nome']
+          },
+          {
+            model: SchoolYearGrade,
+            as: 'turma',
+            attributes: ['turma'],
+          },
+          {
+            model: Period,
+            as: 'period',
+            attributes: ['name']
+          }
+        ]
+      });
+      if (!note) {
+        return res.status(404).json({ error: "Nota não encontrada" });
+      }
+      res.status(200).json(note)
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: "Erro ao buscar nota, tente novamente" });
+    }
+  }
+  async updateNote(req, res) {
+    try {
+      const note = await Note.findByPk(req.params.id);
+      if (!note) {
+        return res.status(404).json({ error: "Nota não encontrada" });
+      }
+      const { value, period_id, user_id, anos_series_id, subject_id } = req.body;
+      const newNotes = await note.update({ value, period_id, user_id, anos_series_id, subject_id })
+      res.status(200).json(newNotes);
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: "Erro ao editar nota, tente novamente" });
+    }
+  }
+  async deleteNote(req, res) {
+    try {
+      const note = await Note.findByPk(req.params.id);
+      if (!note) {
+        return res.status(404).json({ error: "Nota não encontrada" });
+      }
+      await note.destroy();
+      return res.status(204);
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: "Erro ao deletar nota, tente novamente" });
+    }
+  }
 
 }
 
